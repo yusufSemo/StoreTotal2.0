df7c7c1e0fcd7e10d198b7a376eb127466cd84f1
Yusuf Semo <yas3176@mavs.uta.edu>
2024-03-02 01:47:49 -0600

    PDF IText

--- src/main/java/smeo/Main.java
+++ src/main/java/smeo/Main.java
@@ -1,19 +1,72 @@
 package smeo;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws FileNotFoundException, MalformedURLException {
+        String path = "/Users/yusufsemo/Desktop/mypdf.pdf";
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //create an image
+//        String imgPath = "images/birds.jpeg";
+//        ImageData imgData = ImageDataFactory.create(imgPath);
+//        Image imageBirds = new Image(imgData);
+//        imageBirds.scale(0.5f,0.5f);
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        //create a table
+        float columnWidth[] = {100f,100f,100f,100f,100f};
+        Table table = new Table(columnWidth);
+
+        Cell cell_11 = new Cell();
+        cell_11.add(new Paragraph("Date"));
+        table.addCell(cell_11);
+
+        table.addCell(new Cell().add(new Paragraph("Card")));
+        table.addCell(new Cell().add(new Paragraph("Online")));
+        table.addCell(new Cell().add(new Paragraph("Cash")));
+        table.addCell(new Cell().add(new Paragraph("Total")));
+
+        table.addCell(new Cell().add(new Paragraph("10/4/2022")));
+        table.addCell(new Cell().add(new Paragraph("100")));
+        table.addCell(new Cell().add(new Paragraph("200")));
+        table.addCell(new Cell().add(new Paragraph("300")));
+        table.addCell(new Cell().add(new Paragraph("600")));
+
+        table.addCell(new Cell().add(new Paragraph("10/5/2022")));
+        table.addCell(new Cell().add(new Paragraph("200")));
+        table.addCell(new Cell().add(new Paragraph("250")));
+        table.addCell(new Cell().add(new Paragraph("300")));
+        table.addCell(new Cell().add(new Paragraph("750")));
+
+
+
+
+        //create a paragraph
+        String text = "this is a test text test test test";
+        Paragraph testPar = new Paragraph(text);
+
+        PdfWriter pdfWr = new PdfWriter(path);
+        PdfDocument pdfDoc = new PdfDocument(pdfWr);
+        pdfDoc.addNewPage();
+
+        Document document = new Document(pdfDoc);
+        document.add(testPar);
+        //document.add(imageBirds);
+        document.add(table);
+
+        System.out.println("PDF created");
+        document.close();
+
+
     }
 }
